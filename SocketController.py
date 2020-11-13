2d35cef3e5f197a5d444e415d97f40578dcc2da1
Deniro Gayle <deniroturboman@hotmail.com>
2020-11-13 13:47:36 -0500

    Removed socket template and adjusted socket class

--- SocketController.py
+++ SocketController.py
@@ -1,20 +1,5 @@
 import socket
 
-HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
-PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
-
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    conn, addr = s.accept()
-    with conn:
-        print('Connected by', addr)
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            conn.sendall(data)
-
 class SocketController():
     """ A simple socket abstraction class to remove complexity elsewhere """
     def __init__(self, host, port, bufferLen = 1024):
@@ -32,10 +17,9 @@ class SocketController():
         return 0
     def Listen(self):
         self.ssocket.listen()
-        conn, addr = self.ssocket.accept()
-        return conn, addr
-    def SendData(self, msg):
-        return 0
-    def RecvData(self):
+        return self.ssocket.accept()
+    def SendData(self, msg: str):
+        return self.ssocket.send(msg)
+    def RecvData(self, conn):
         return self.ssocket.recv(self.buffLen)
     pass
