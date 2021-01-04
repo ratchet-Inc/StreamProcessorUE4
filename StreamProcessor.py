0f6a81c20869af56521be6cdc3bc2d1c8cbf9967
Deniro Gayle <deniroturboman@hotmail.com>
2021-01-04 15:00:44 -0500

    improved main jpeg stream to continously stream data

--- StreamProcessor.py
+++ StreamProcessor.py
@@ -10,6 +10,7 @@ import FrameParser
 import time
 import StreamTest as ST
 import DatabaseInterface
+import math
 
 def ParseArgs():
     args = JSON.loads('{"-host": "127.0.0.1", "-port":8000 }') # default arguments
@@ -83,21 +84,58 @@ def TestFunc():
     ParseData2(r)
     return 0
 
+def CalcSegments(msgLen):
+    # base segment is 1 | base sleep time is 16.667 ms
+    # since maximum UDP packet includes header data I'm using
+    # 32000 bytes as divisor instead of the full 32Kb
+    data = [1, 0.1667, msgLen]
+    quotient = math.ceil(msgLen / 32000)
+    if quotient > 1:
+        data[2] = 32000
+        pass
+    data[0] = quotient
+    data[1] = data[1] // quotient   # get the lower bound
+    return data
+
 def main_JPEG_Stream(args):
     db = DatabaseInterface.DB(cname = "stream", creg = True, cinit = True)
     res = db.Init()
     if(res != 0):
         print("*failed to connect to database.")
         return 1
+    print('RCVBUF={}'.format(
+        DatabaseInterface.API.UDP_Socket.getsockopt(
+            SocketController.socket.SOL_SOCKET,
+            SocketController.socket.SO_RCVBUF
+            )
+        )
+    )
     sock = SocketController.SocketController(args['-host'], args['-port'], args['-bsize'])
     sock.InitSocket_Server()
     print("*stream processor is listening..")
     conn, addr = sock.Listen()
     print("Connected by Address: ", addr)
+    frameCounter = 0
     while(True):
+        # caviot: appearantly my encryption method increases the size of the messages
+        # so my UDP messages end up being larger than 64Kb, so I have to send the message in segments
         recv = sock.RecvData(conn)
-        db.SendData(recv)
-        #time.sleep(0.1667)
+        recvLen = len(recv)
+        print("recieved: %s | bytes: %s." % (recv, recvLen))
+        segData = CalcSegments(recvLen)
+        for x in range(segData[0]):
+            lowerBound = segData[2] * x
+            upperBound = segData[2] * x + 1
+            if x == (segData[0] - 1):
+                upperBound = recvLen
+                pass
+            print("segment[%s:%s]: %s" % (lowerBound, upperBound, recv[lowerBound:upperBound]))
+            db.SendData(recv[lowerBound:upperBound])
+            time.sleep(segData[1])
+            pass
+        db.SendData("END FRAME:{}".format(frameCounter))
+        break
+        frameCounter += 1
         pass
     sock.Close()
     db.Close()
